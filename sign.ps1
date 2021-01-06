17fc411215d49e73ceecbac4115c6381c3c9109b
Jingchao Feng <9412032+fjch1997@users.noreply.github.com>
2021-01-06 09:32:59 -0500

    Update sign.ps1

--- sign.ps1
+++ sign.ps1
@@ -15,10 +15,12 @@ foreach ($msixFile in Get-ChildItem "ShowTractor.WinUI\ShowTractor.WinUI (Packag
         $exeName = $exeFile.Name
         $exePath = $exeFile.FullName
         Write-Host "Signing $exeName"
-        Set-AuthenticodeSignature -Certificate $certificate -FilePath $exePath -TimestampServer http://timestamp.sectigo.com
+        Set-AuthenticodeSignature -Certificate $certificate -TimestampServer http://timestamp.sectigo.com -FilePath $exePath
     }
     # Change the package identity name so as to not conflict with debug installation.
     (Get-Content "$unpackedPath\AppxManifest.xml").Replace("f0d5fe17-7d4a-4795-9ab2-55b4be65317e", "ff67f6ff-3707-446e-a79d-3e95f4d04f68") | Out-File "$unpackedPath\AppxManifest.xml" -Encoding utf8
     MakeAppx pack /d $unpackedPath /p $msixPath /o
     SignTool sign /a /v /fd SHA256 /tr http://timestamp.sectigo.com /td SHA256 /sha1 $certificate.Thumbprint $msixPath
 }
+# Sign plugins
+Set-AuthenticodeSignature -Certificate $certificate -TimestampServer http://timestamp.sectigo.com -FilePath ShowTractor.Plugins.Tmdb\bin\Release\netstandard2.1\ShowTractor.Plugins.Tmdb.dll
