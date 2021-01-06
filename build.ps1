871848603c516147f3befa93b7edc6e3a68178ff
Jingchao Feng <fjch1997@outlook.com>
2021-01-05 19:43:03 -0500

    Build scripts

---
+++ build.ps1
@@ -0,0 +1,6 @@
+﻿msbuild ShowTractor.sln /t:Restore /p:Configuration=Release
+dotnet test ShowTractor.Tests
+dotnet test ShowTractor.Plugins.Tmdb.Tests
+msbuild ShowTractor.sln /p:Configuration=Release /p:Platform=x86
+msbuild ShowTractor.sln /p:Configuration=Release /p:Platform=x64
+msbuild ShowTractor.sln /p:Configuration=Release /p:Platform=arm64
---
+++ sign.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = "Stop"
+$certificate = (Get-Item Cert:\CurrentUser\My\* -CodeSigningCert)
+Write-Host "Signing will be using" $certificate.FriendlyName
+foreach ($msixFile in Get-ChildItem "ShowTractor.WinUI\ShowTractor.WinUI (Package)\AppPackages\ShowTractor.WinUI (Package)*\*.msix")
+{
+    $msixPath = $msixFile.FullName
+    Write-Host "Signing Package $msixPath"
+    $unpackedPath = $msixFile.FullName.Substring(0, $msixFile.FullName.Length - 5)
+    MakeAppx unpack /o /p $msixPath /d $unpackedPath
+    Get-ChildItem $unpackedPath\
+    foreach ($exeFile in Get-ChildItem "$unpackedPath\**\*.exe")
+    {
+        $exeName = $exeFile.Name
+        $exePath = $exeFile.FullName
+        Write-Host "Signing $exeName"
+        Set-AuthenticodeSignature -Certificate $certificate -FilePath $exePath -TimestampServer http://timestamp.sectigo.com
+    }
+    MakeAppx pack /d $unpackedPath /p $msixPath /o
+    SignTool sign /a /v /fd SHA256 /tr http://timestamp.sectigo.com /td SHA256 /sha1 $certificate.Thumbprint $msixPath
+}
