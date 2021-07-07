f4de20d4e875f4cc25c3c0315ec804864c9d8ba9
Jingchao Feng <fjch1997@outlook.com>
2021-07-06 20:07:53 -0400

    IMediaPlayer support out of process player

--- ShowTractor.Plugins/IMediaPlayer.cs
+++ ShowTractor.Plugins/IMediaPlayer.cs
@@ -1,16 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace ShowTractor.Plugins.Interfaces
 {
+    /// <summary>
+    /// The type of media player that an <see cref="IMediaPlayer"/> implementation use.
+    /// </summary>
+    public enum MediaPlayerMode
+    {
+        /// <summary>
+        /// <see cref="IMediaPlayer"/> will launch a player in a new window or process on its own. 
+        /// <see cref="ShowTractor"/> will not handle any display.
+        /// </summary>
+        Process,
+        /// <summary>
+        /// <see cref="IMediaPlayer"/> will return a ControlTemplate (The actual underlying type depends on the platform) 
+        /// to be displayed in the Main Window. 
+        /// </summary>
+        ControlTemplate,
+    }
+
+    /// <summary>
+    /// A superset of <see cref="MediaSourceType"/> that contain the mode an <see cref="IMediaPlayer"/> will use.
+    /// </summary>
+    public struct MediaPlayerSourceType
+    {
+        public Guid Id { get; set; }
+        public string DisplayName { get; set; }
+        public MediaPlayerMode Mode { get; set; }
+    }
+
+    public enum MediaPlayerState
+    {
+        Playing,
+        Paused,
+        Closed,
+    }
+
+    public class MediaPlayerStateViewModel : INotifyPropertyChanged
+    {
+        public MediaPlayerState State { get => state; set { state = value; OnPropertyChanged(); } }
+        private MediaPlayerState state;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
     public interface IMediaPlayer : IPlugin
     {
-        public IEnumerable<MediaSourceType> SupportedTypes { get; set; }
+        public IEnumerable<MediaPlayerSourceType> SupportedTypes { get; set; }
         /// <summary>
         /// Get a ControlTemplate that can be loaded as a player. The actual underlying type depends on the platform.
+        /// This method is called only when the reported mode is <see cref="MediaPlayerMode.ControlTemplate"/>.
         /// </summary>
-        /// <returns></returns>
         public ValueTask<object> GetPlayerAsync(MediaSource source, TvSeason tvSeason, TvEpisode tvEpisode, Action closePlayerCallback);
+        /// <summary>
+        /// Get a <see cref="MediaPlayerStateViewModel"/> to track the state of a media player opened in a new window or process.
+        /// This method is called only when the reported mode is <see cref="MediaPlayerMode.Process"/>.
+        /// </summary>
+        public ValueTask<MediaPlayerStateViewModel> PlayAsync(MediaSource source, TvSeason tvSeason, TvEpisode tvEpisode);
     }
 }
--- ShowTractor.Plugins/IMediaSourceProvider.cs
+++ ShowTractor.Plugins/IMediaSourceProvider.cs
@@ -2,8 +2,14 @@
 
 namespace ShowTractor.Plugins.Interfaces
 {
+    /// <summary>
+    /// Implementations of this interface are singleton and must be thread safe.
+    /// </summary>
     public interface IMediaSourceProvider : IPlugin
     {
+        /// <summary>
+        /// Implementations of this interface are singleton and must be thread safe.
+        /// </summary>
         IAsyncEnumerable<MediaSource> GetAsync(TvSeason tvSeason, TvEpisode tvEpisode);
     }
 }
