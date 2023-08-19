e4fcc74b456a6276eec5851d4ccc905e47982adb
Jingchao Feng <fjch1997@outlook.com>
2023-08-19 16:45:09 -0700

    Fix a bug where it fails to follow new seasons if database size is larger than brand new because of sqlite "database is locked" error

--- ShowTractor/Background/MetadataUpdateBackgroundWork.cs
+++ ShowTractor/Background/MetadataUpdateBackgroundWork.cs
@@ -3,6 +3,7 @@ using ShowTractor.Extensions;
 using ShowTractor.Interfaces;
 using ShowTractor.Plugins.Interfaces;
 using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Threading;
@@ -39,59 +40,69 @@ namespace ShowTractor.Background
             var shows = context.TvSeasons
                 .Include(s => s.AdditionalAttributes)
                 .Include(s => s.Episodes).OrderBy(s => s.Season).AsAsyncEnumerable().GroupBy(s => s.ShowName);
-            await foreach (var show in shows)
+            var numberOfSeasonsAdded = 0;
+			await foreach (var show in shows)
             {
-                GetLatestSeasonsDelegate? getLatestSeasonsDelegate = null;
-                int lastSeasonNumber = 0;
-                await foreach (var dbSeason in show)
+                var showName = "Uninitialized";
+                try
                 {
-                    (var latest, getLatestSeasonsDelegate) = await provider.GetUpdatesAsync(dbSeason, token);
-                    await dbSeason.UpdateAsync(latest, httpClient);
-                    for (int i = 0; i < latest.Episodes.Count; i++)
+                    GetLatestSeasonsDelegate? getLatestSeasonsDelegate = null;
+                    int lastSeasonNumber = 0;
+                    await foreach (var dbSeason in show)
                     {
-                        if (dbSeason.Episodes == null) throw new Exception($"{nameof(dbSeason.Episodes)} is null.");
-                        if (i >= dbSeason.Episodes.Count)
+                        showName = dbSeason.ShowName;
+                        (var latest, getLatestSeasonsDelegate) = await provider.GetUpdatesAsync(dbSeason, token);
+                        await dbSeason.UpdateAsync(latest, httpClient);
+                        for (int i = 0; i < latest.Episodes.Count; i++)
                         {
-                            dbSeason.Episodes.Add(Database.TvEpisode.FromRecord(latest.Episodes[i]));
+                            if (dbSeason.Episodes == null) throw new Exception($"{nameof(dbSeason.Episodes)} is null.");
+                            if (i >= dbSeason.Episodes.Count)
+                            {
+                                dbSeason.Episodes.Add(Database.TvEpisode.FromRecord(latest.Episodes[i]));
+                            }
+                            else
+                            {
+                                await dbSeason.Episodes[i].UpdateAsync(latest.Episodes[i], httpClient);
+                            }
                         }
-                        else
+                        lastSeasonNumber = dbSeason.Season;
+                    }
+                    if (getLatestSeasonsDelegate != null)
+                    {
+                        await foreach (var newSeason in getLatestSeasonsDelegate(lastSeasonNumber))
                         {
-                            await dbSeason.Episodes[i].UpdateAsync(latest.Episodes[i], httpClient);
+                            numberOfSeasonsAdded++;
+                            var dbSeason = Database.TvSeason.FromRecord(newSeason ?? throw new ArgumentNullException(nameof(newSeason)), provider.GetAssemblyName());
+                            if (dbSeason.Artwork == null && newSeason.ArtworkUri != null)
+                            {
+                                try
+                                {
+                                    dbSeason.Artwork = await httpClient.GetByteArrayAsync(newSeason.ArtworkUri);
+                                }
+                                catch (Exception ex)
+                                {
+                                    throw new MetadataUpdateBackgroundWorkArtworkLoadException(ex);
+                                }
+                            }
+                            context.TvSeasons.Add(dbSeason);
+                            dbSeason.Following = true;
+                            dbSeason.DateFollowed = DateTime.SpecifyKind(DateTime.Today, DateTimeKind.Utc);
+                            foreach (var episode in newSeason.Episodes)
+                            {
+                                var dbEpisode = Database.TvEpisode.FromRecord(episode);
+                                dbEpisode.TvSeasonId = dbSeason.Id;
+                                await dbEpisode.UpdateAsync(episode, httpClient);
+                                await context.TvEpisodes.AddAsync(dbEpisode);
+                            }
                         }
                     }
-                    lastSeasonNumber = dbSeason.Season;
                 }
-                if (getLatestSeasonsDelegate != null)
+                catch (Exception ex)
                 {
-                    await foreach (var newSeason in getLatestSeasonsDelegate(lastSeasonNumber))
-                    {
-                        var dbSeason = Database.TvSeason.FromRecord(newSeason ?? throw new ArgumentNullException(nameof(newSeason)), provider.GetAssemblyName());
-                        if (dbSeason.Artwork == null && newSeason.ArtworkUri != null)
-                        {
-                            try
-                            {
-                                dbSeason.Artwork = await httpClient.GetByteArrayAsync(newSeason.ArtworkUri);
-                            }
-                            catch (Exception ex)
-                            {
-                                throw new MetadataUpdateBackgroundWorkArtworkLoadException(ex);
-                            }
-                        }
-                        context.TvSeasons.Add(dbSeason);
-                        dbSeason.Following = true;
-                        dbSeason.DateFollowed = DateTime.SpecifyKind(DateTime.Today, DateTimeKind.Utc);
-                        await Task.Run(async () => await context.SaveChangesAsync());
-                        foreach (var episode in newSeason.Episodes)
-                        {
-                            var dbEpisode = Database.TvEpisode.FromRecord(episode);
-                            dbEpisode.TvSeasonId = dbSeason.Id;
-                            await dbEpisode.UpdateAsync(episode, httpClient);
-                            await context.TvEpisodes.AddAsync(dbEpisode);
-                        }
-                        await Task.Run(async () => await context.SaveChangesAsync());
-                    }
+                    Trace.TraceWarning($"Bcakground update failed for {showName}\r\n{ex}");
                 }
             }
+            Trace.TraceInformation($"Follwoed {numberOfSeasonsAdded} new seasons.");
             await context.SaveChangesAsync(token);
         }
     }
