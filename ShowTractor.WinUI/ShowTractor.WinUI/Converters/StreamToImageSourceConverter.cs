7a30b2e770af26c0e649517e68e3de00e07a7986
Jingchao Feng <fjch1997@outlook.com>
2025-09-17 15:28:03 -0700

    Fix plugin page

---
+++ ShowTractor.WinUI/ShowTractor.WinUI/Converters/StreamToImageSourceConverter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media.Imaging;
+using ShowTractor.Interfaces;
+using System;
+using System.IO;
+
+namespace ShowTractor.WinUI.Converters
+{
+    public class StreamToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var stream = (IFactory<Stream>)value;
+            if (stream != null)
+            {
+                var image = new BitmapImage();
+                image.SetSource(stream.Get().AsRandomAccessStream());
+                return image;
+            };
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
