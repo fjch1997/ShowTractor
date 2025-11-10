02945bd57285fb60b3cba8f8b6601cde772c2ca3
Jingchao Feng <fjch1997@outlook.com>
2025-11-10 01:27:35 -0800

    GeneralSettingsPage except commands

--- ShowTractor/Pages/Settings/GeneralSettingsPageViewModel.cs
+++ ShowTractor/Pages/Settings/GeneralSettingsPageViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using ShowTractor.Mvvm;
 using ShowTractor.Properties;
+using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Input;
 
 namespace ShowTractor.Pages.Settings
 {
@@ -10,18 +14,38 @@ namespace ShowTractor.Pages.Settings
         internal GeneralSettingsPageViewModel(GeneralSettings settings)
         {
             Settings = settings;
-        }
+            var stringBuilder = new StringBuilder(128);
+            StrFormatByteSize(new FileInfo(settings.DatabaseFilename).Length, stringBuilder, stringBuilder.Capacity);
+            DatabaseSize = stringBuilder.ToString();
+            LoadCommand = new DelegateCommand(() =>
+            {
 
+            });
+            MoveCommand = new DelegateCommand(() =>
+            {
+
+            });
+            ChangeCommand = new DelegateCommand(() =>
+            {
+
+            });
+        }
         public GeneralSettings Settings { get; }
         public string Version => Resources.VersionColon + assembly.GetName().Version?.ToString();
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Used by generated files.")]
         public string Author => Resources.AuthorColonAuthorName;
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Used by generated files.")]
         public string Copyright => Resources.CopyrightColonCopyrightInfo;
+        public ICommand LoadCommand { get; set; }
+        public ICommand MoveCommand { get; set; }
+        public ICommand ChangeCommand { get; set; }
         public object? Parameter { get => null; set { } }
+        public string DatabaseSize { get; private set; }
         public void OnNavigatedFrom()
         {
             Settings.Save();
         }
+        [DllImport("Shlwapi.dll", CharSet = CharSet.Auto)]
+        public static extern long StrFormatByteSize(long fileSize, [MarshalAs(UnmanagedType.LPTStr)] StringBuilder buffer, int bufferSize);
     }
 }
