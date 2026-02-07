a9959f96f5667647e8991c59fc63338e663c18c7
Jingchao Feng <fjch1997@outlook.com>
2026-02-07 11:47:06 -0800

    Media source provider interfaces

---
+++ ShowTractor.Plugins/IMediaSourceProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ShowTractor.Plugins.Interfaces
+{
+    public interface IMediaSourceProvider : IPlugin
+    {
+        bool CanProvide(TvSeason season, TvEpisode episode);
+        IAsyncEnumerable<MediaSource> GetMediaSourcesAsync(TvSeason season, TvEpisode episode);
+    }
+}
---
+++ ShowTractor.Plugins/MediaSource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ShowTractor.Plugins.Interfaces
+{
+    public class MediaSource
+    {
+        public string ShowName { get; set; } = string.Empty;
+        public int Season { get; set; }
+        public int EpisodeNumber { get; set; }
+        /// <summary>
+        /// Whether the media source is immediately playable. If it is, a play button should be directly shown in the UI.
+        /// </summary>
+        public bool Playable { get; set; }
+        /// <summary>
+        /// The remote URI of the media source. The protocol will be used to determine which Download Manager or Media Player to use.
+        /// </summary>
+        public Uri? Uri { get; set; }
+        /// <summary>
+        /// The size of the media in bytes. If zero, size will be hidden in the UI.
+        /// </summary>
+        public long Bytes { get; set; }
+    }
+}
